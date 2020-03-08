962d4f5c3997cba7798d89f7f153aad28882454f
GeenStack <hayleytops@gmail.com>
2020-03-09 01:16:47 +0300

    first modified

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
 command_center_ip = '' #command_center_URL -> http://ip(or domain name)
+last_message = '' 	   #URL to get last message (without ip and protocol)
+push_message = ''      #URL to pushing message (without ip and protocol)
